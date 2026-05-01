0159c9f3b52e5e78b5594aaee60a45db09787095
fernanda jimenez ramos <A01645485@tec.mx>
2026-05-01 12:46:18 -0600

    activity done

---
+++ sql_challenges/challenge-07/solution.sql
@@ -0,0 +1,26 @@
+-- 1 What is a grand slam?
+
+CHUNK_ID	PREVIEW	                                                                                SIMILARITY_SCORE
+21	Una categoría imaginaria de ganadores de Grand Slam puede crearse con aquellos tenistas que hayan ga	0.3198
+1	El Grand Slam de tenis está constituido por los cuatro torneos oficiales mayores del circuito intern	0.3787
+11	Aunque la denominación de Grand Slam se acuñó originalmente para aquellos jugadores que ganaran esos	0.4075
+
+
+Answer makes sense, but chunk limit doesn't allow me to view the whole paragraph
+
+-- 2 What is tennis?
+
+CHUNK_ID	PREVIEW	SIMILARITY_SCORE
+22	Serena Williams estuvo muy cerca de unirse a este exclusivo club, ya que solo le faltó ganar los dob	0.5805
+12	Nota: El Australian Open se disputó en diciembre desde 1977 hasta 1985, tras lo cual volvió a su ubi	0.595
+19	Por otra parte, a Roger Federer le faltó el oro para poder completar el Golden Slam, así como a Chri	0.6029
+
+Answers dont make sense, i asked about the sport, not just the most relevant new about the sport 
+
+-- 3 Who is Fernanda Jimenez?
+
+CHUNK_ID	PREVIEW	SIMILARITY_SCORE
+13	Gigi Fernández/Natasha Zvereva (1992–93, seis consecutivos) - (Roland Garros 1992, Wimbledon 1992, U	0.6944
+14	Además Natasha Zvereva (1996–97), ganó junto a Gigi Fernández, Wimbledon 1996, el US Open 1996 y Rol	0.7066
+6	Se consideran checos los títulos ganados por jugadores de esa nacionalidad a partir de 1993, tras la	0.7711
+Names match but I am not a tennis player 😅
